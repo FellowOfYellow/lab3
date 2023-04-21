85fb915fd52d1e43a0f015180b5e6ad0977a963c
FellowOfYellow <64289766+FellowOfYellow@users.noreply.github.com>
2023-04-20 19:05:49 -0700

    Fixed Reveresed & ReversedInPlace

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,36 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+
+  @Test 
+	public void testReverseInPlace2() {
+    int[] input1 = { 3,4,5,6 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{ 6,5,4,3 }, input1);
 	}
 
+  @Test 
+	public void testReversed2() {
+    int[] input1 = { 3,4,5,6,7 };
+    assertArrayEquals(new int[]{ 7,6,5,4,3 }, ArrayExamples.reversed(input1));
+	}
 
   @Test
-  public void testReversed() {
+  public void testReversed1() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+// @Test
+// public void testAverageWithoutLowest1() {
+//   double[] input1 = {1};
+//   assertEquals((double)0, ArrayExamples.averageWithoutLowest(input1),0);
+// }
+
+// @Test
+// public void testAverageWithoutLowest2() {
+//   double[] input1 = {5,10,15,25};
+//   assertEquals((double)25, ArrayExamples.averageWithoutLowest(input1),0);
+// }
+
 }
